90d9203ee07c87a17dd5ca21ddb68c24a07e2cd8
Jordan <19174130+jordan-git@users.noreply.github.com>
2018-08-04 10:34:30 +0100

    General clean-up

--- cmds/admin.py
+++ cmds/admin.py
@@ -1,6 +1,4 @@
-from discord.ext import commands
-from discord import Member
-from server import server
+from discord.ext.commands import command, Context
 import json
 import subprocess
 import asyncio
@@ -17,86 +15,90 @@ class Admin(object):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True)
-    async def a(self, ctx, *, msg : str):
+    @command(pass_context=True)
+    async def a(self, ctx: Context, *, msg: str):
         if not util.is_admin(ctx.message.author):
             return await self.bot.say("You are not an administrator.")
 
         out = json.dumps({
-            "type":"asay",
-            "sender":str(ctx.message.author.display_name),
-            "message":msg
+            "type": "asay",
+            "sender": str(ctx.message.author.display_name),
+            "message": msg
         })
+
         await util.send_check(self.bot, ctx.message, out)
 
-
-    @commands.command(pass_context=True)
-    async def admins(self, ctx):
+    @command(pass_context=True)
+    async def admins(self, ctx: Context):
         channel = ctx.message.channel
+
         out = json.dumps({
-            "type":"admins",
-            "channel":str(channel.id)
+            "type": "admins",
+            "channel": str(channel.id)
         })
+
         await util.send_check(self.bot, ctx.message, out)
 
-    @commands.command(pass_context=True)
-    async def prison(self, ctx,  player : str, ptime : int, *, reason : str):
-
+    @command(pass_context=True)
+    async def prison(self, ctx: Context,  player: str, ptime: int, *, reason: str):
         if not util.is_admin(ctx.message.author):
             return await self.bot.say("You are not an administrator.")
+
         out = json.dumps({
-            "type":"prison",
-            "sender":str(ctx.message.author.display_name),
-            "player":player,
+            "type": "prison",
+            "sender": str(ctx.message.author.display_name),
+            "player": player,
             "ptime": ptime,
             "reason": reason
         })
+
         await util.send_check(self.bot, ctx.message, out)
 
+    @command(pass_context=True)
+    async def getlogs(self, ctx: Context, pattern: str):
 
-
-    @commands.command(pass_context=True)
-    async def getlogs(self, ctx, pattern : str):
         admin_level = util.is_admin(ctx.message.author)
-        if admin_level is "Probie" or not admin_level:
+        if admin_level is "Probie":
+            return await self.bot.say("Probationary admins cannot use this feature.")
+        if not admin_level:
             return await self.bot.say("You are not an administrator.")
 
         cmd = ['grep', '-m 3000', '-E', pattern, '/home/sarp/samp03z/server_log.txt']
         with open('./files/log.txt', 'wb') as logf:
-            subprocess.Popen(cmd, stdout=logf, shell=False) #shell=False to avoid shell injection
+            subprocess.Popen(cmd, stdout=logf, shell=False)  # shell=False to avoid shell injection
+
         await asyncio.sleep(2)
         await self.bot.upload('./files/log.txt')
 
-    
-
-    @commands.command(pass_context=True)
-    async def kick(self, ctx,  player : str, *, reason : str):
-        
+    @command(pass_context=True)
+    async def kick(self, ctx: Context,  player: str, *, reason: str):
         if not util.is_admin(ctx.message.author):
             return await self.bot.say("You are not an administrator.")
+
         out = json.dumps({
-            "type":"kick",
-            "sender":str(ctx.message.author.display_name),
-            "player":player,
+            "type": "kick",
+            "sender": str(ctx.message.author.display_name),
+            "player": player,
             "reason": reason
         })
+
         await util.send_check(self.bot, ctx.message, out)
 
-
-    @commands.command(pass_context=True)
-    async def w(self, ctx,  player : str, *, message : str):
-        
+    @command(pass_context=True)
+    async def w(self, ctx: Context,  player: str, *, message: str):
         if not util.is_admin(ctx.message.author):
             return await self.bot.say("You are not an administrator.")
-            
+
         out = json.dumps({
-            "type":"w",
-            "sender":str(ctx.message.author.display_name),
-            "player":player,
+            "type": "w",
+            "sender": str(ctx.message.author.display_name),
+            "player": player,
             "message": message
         })
+
         await util.send_check(self.bot, ctx.message, out)
-    """Temporary removing this
+
+    """ Temporarily removing this
     @commands.command(pass_context=True)
     async def stats(self, ctx, *, user : str):
         out = json.dumps({
@@ -105,6 +107,7 @@ class Admin(object):
         })
         await util.send_check(self.bot, ctx.message, out)
     """
+
 
 # this is important, this basically creates a new object of Admin
 def setup(bot):
