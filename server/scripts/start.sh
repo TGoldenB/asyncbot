546cc2de9a93d48c7a03e6c566f93a1a61ae3181
Dylan Tonks <deejtonks@gmail.com>
2018-09-12 12:49:14 +1200

    The start.sh bash script now restarts the bot effectively

--- server/scripts/start.sh
+++ server/scripts/start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-pkill python3
-nohup python3 -u ./main.py > log.txt &
-tail -f log.txt
+if [ -f bot.pid ]; then
+        kill -9 `cat bot.pid`
+        rm bot.pid
+fi
+if [ -f logs/log.txt ]; then
+        fmt="$(date)"
+        fmt="$(date +'%d-%m')"
+        mv "logs/log.txt" "logs/$fmt-log.txt"
+fi
+nohup python3 -u ./main.py > logs/log.txt 2>&1 &
+echo $! > bot.pid
