3c446c01737b3e1984c14ad9742e29de74a16380
Dylan Tonks <deejtonks@gmail.com>
2018-09-12 16:37:38 +1200

    Update start.sh

--- server/scripts/start.sh
+++ server/scripts/start.sh
@@ -10,3 +10,7 @@ if [ -f logs/log.txt ]; then
 fi
 nohup python3 -u ./main.py > logs/log.txt 2>&1 &
 echo $! > bot.pid
+chown $USER:sarpadmins bot.pid
+chown $USER:sarpadmins "logs/$fmt-log.txt"
+chmod 770 bot.pid
+chmod 770 "logs/$fmt-log.txt"
