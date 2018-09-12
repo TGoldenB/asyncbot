8655ffef623887b3e88c6091186eb533856a7af5
Dylan Tonks <deejtonks@gmail.com>
2018-09-12 16:38:00 +1200

    Update start_noperm.sh

--- server/scripts/start_noperm.sh
+++ server/scripts/start_noperm.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # just a script to start the bot without killing it, use this if you do not have permission to kill
-python3 terminate.py
+#!/bin/bash
 if [ -f logs/log.txt ]; then
         fmt="$(date)"
         fmt="$(date +'%d-%m')"
         mv "logs/log.txt" "logs/$fmt-log.txt"
 fi
 nohup python3 -u ./main.py > logs/log.txt 2>&1 &
+rm bot.pid
 echo $! > bot.pid
+chown $USER:sarpadmins bot.pid
+chown $USER:sarpadmins "logs/$fmt-log.txt"
+chmod 770 bot.pid
+chmod 770 "logs/$fmt-log.txt"
